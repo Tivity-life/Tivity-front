a7037bbd918b5ad6ab431cab922a98f1771bd9f9
Damiano Gambitta <damianogambitta@gmail.com>
2021-05-19 15:36:38 +0200

    Calendar Route

--- src/router/index.js
+++ src/router/index.js
@@ -3,61 +3,61 @@ import HomePage from '../components/home/home.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: HomePage
+    path: "/",
+    name: "Home",
+    component: HomePage,
   },
   {
-    path: '/sign-in',
-    name: 'sign-in',
-    component: () => import('../components/sign-in/signin.vue')
+    path: "/sign-in",
+    name: "sign-in",
+    component: () => import("../components/sign-in/signin.vue"),
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('../components/login/login.vue')
+    path: "/login",
+    name: "login",
+    component: () => import("../components/login/login.vue"),
   },
   {
-    path: '/todolist',
-    name: 'todolist',
-    component: () => import('../components/todolist/todo.vue')
+    path: "/todolist",
+    name: "todolist",
+    component: () => import("../components/todolist/todo.vue"),
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('../components/home/about.vue')
+    path: "/about",
+    name: "about",
+    component: () => import("../components/home/about.vue"),
   },
   {
-    path: '/start',
-    name: 'start',
-    component: () => import('../components/start/start.vue')
+    path: "/start",
+    name: "start",
+    component: () => import("../components/start/start.vue"),
   },
   {
-    path: '/to-do',
-    name: 'to-do',
-    component: () => import('../components/todolist/todo.vue')
+    path: "/to-do",
+    name: "to-do",
+    component: () => import("../components/todolist/todo.vue"),
   },
   {
-    path: '/map',
-    name: 'map',
-    component: () => import('../components/map/map.vue')
+    path: "/map",
+    name: "map",
+    component: () => import("../components/map/map.vue"),
   },
   {
-    path: '/habits',
-    name: 'habits',
-    component: () => import('../components/habit-tracker/habit.vue')
+    path: "/habits",
+    name: "habits",
+    component: () => import("../components/habit-tracker/habit.vue"),
   },
   {
-    path: '/calendar',
-    name: 'calendar',
-    component: () => import('../views/Calendar.vue')
+    path: "/calendar",
+    name: "calendar",
+    component: () => import("../components/calendar/Calendar.vue"),
   },
   {
-    path: '/homepage',
-    name: 'homepage',
-    component: () => import('../components/homepage/homepage.vue')
-  }
-]
+    path: "/homepage",
+    name: "homepage",
+    component: () => import("../components/homepage/homepage.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
