5160f0867690e284a98dc2f7b8f337d96ce7e611
ChiaraGiaca <gia_chiara@yahoo.it>
2021-05-21 12:41:26 +0200

    fixed responsive

--- src/components/habit-tracker/habit.vue
+++ src/components/habit-tracker/habit.vue
@@ -1,5 +1,5 @@
 <template>
-<div v-if="isMobile()" class= "container">
+<div :class="[isMobile() ? 'container' : '']">
   <h2 style="padding: 10px; text-align:center">Il mio habit tracker settimanale <span style="font-size: 30px">🏡</span> </h2>
   <header style="justify-content:center; text-align:center">
   <img src= "../../assets/prova3.jpg"/>
@@ -207,214 +207,7 @@
     </div>
   </div>
 </div>
-<div v-else>
-   <h2 style="padding: 10px; text-align:center">Il mio habit tracker settimanale <span style="font-size: 30px">🏡</span> </h2>
-  <header style="justify-content:center; text-align:center">
-  <img src= "../../assets/prova3.jpg"/>
-  </header><br>
-  <p style= "text-align:center">
-    Nelle righe in orizzontale ci sono i giorni, per ogni settimana.<br />
-    Nelle righe in verticale ci sono le <strong>abitudini</strong> che puoi
-    inserire per essere coerente con il tipo di persona che vuoi diventare
-    <em>“da grande”</em>. <br />Alcuni esempio: tempo di qualità in famiglia,
-    pulizie di casa, attività fisica, mangiare bene, bere acqua, leggere,
-    studiare, disegnare, meditare, ecc.<br>Inserisci tutto ciò che vuoi, e visualizza
-    le statistiche settimanali nel grafico sottostante, cliccando sulla freccia a destra!
-  </p>
-  <br />
-  <div id="habits" class="table-responsive">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Inserisci le abitudini che vuoi coltivare 🌱</th>
-          <th>Lunedì</th>
-          <th>Martedì</th>
-          <th>Mercoledì</th>
-          <th>Giovedì</th>
-          <th>Venerdì</th>
-          <th>Sabato</th>
-          <th>Domenica</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(days, i) in weekDaysByStatus" :key="i">
-          <td>
-            <input
-              id="name"
-              class="form-control"
-              type="text"
-              v-model="weekDays[i].insert"
-            />
-          </td>
 
-          <th>
-            <button
-              :class="[
-                weekDays[i].isActiveMonday
-                  ? 'btn btn-success'
-                  : 'btn btn-warning btn-rounded',
-              ]"
-              @click="weekDays[i].isActiveMonday = !weekDays[i].isActiveMonday"
-            >
-              {{ weekDays[i].isActiveMonday ? "Fatto!" : "Da fare" }}
-            </button>
-          </th>
-          <td>
-            <button
-              :class="[
-                weekDays[i].isActiveTuesday
-                  ? 'btn btn-success'
-                  : 'btn btn-warning btn-rounded',
-              ]"
-              @click="
-                weekDays[i].isActiveTuesday = !weekDays[i].isActiveTuesday
-              "
-            >
-              {{ weekDays[i].isActiveTuesday ? "Fatto!" : "Da fare" }}
-            </button>
-          </td>
-          <td>
-            <button
-              :class="[
-                weekDays[i].isActiveWednesday
-                  ? 'btn btn-success'
-                  : 'btn btn-warning btn-rounded',
-              ]"
-              @click="
-                weekDays[i].isActiveWednesday = !weekDays[i].isActiveWednesday
-              "
-            >
-              {{ weekDays[i].isActiveWednesday ? "Fatto!" : "Da fare" }}
-            </button>
-          </td>
-          <td>
-            <button
-              :class="[
-                weekDays[i].isActiveThursday
-                  ? 'btn btn-success'
-                  : 'btn btn-warning btn-rounded',
-              ]"
-              @click="
-                weekDays[i].isActiveThursday = !weekDays[i].isActiveThursday
-              "
-            >
-              {{ weekDays[i].isActiveThursday ? "Fatto!" : "Da fare" }}
-            </button>
-          </td>
-          <td>
-            <button
-              :class="[
-                weekDays[i].isActiveFriday
-                  ? 'btn btn-success'
-                  : 'btn btn-warning btn-rounded',
-              ]"
-              @click="weekDays[i].isActiveFriday = !weekDays[i].isActiveFriday"
-            >
-              {{ weekDays[i].isActiveFriday ? "Fatto!" : "Da fare" }}
-            </button>
-          </td>
-          <td>
-            <button
-              :class="[
-                weekDays[i].isActiveSaturday
-                  ? 'btn btn-success'
-                  : 'btn btn-warning btn-rounded',
-              ]"
-              @click="
-                weekDays[i].isActiveSaturday = !weekDays[i].isActiveSaturday
-              "
-            >
-              {{ weekDays[i].isActiveSaturday ? "Fatto!" : "Da fare" }}
-            </button>
-          </td>
-          <td>
-            <button
-              :class="[
-                weekDays[i].isActiveSunday
-                  ? 'btn btn-success'
-                  : 'btn btn-warning btn-rounded',
-              ]"
-              @click="weekDays[i].isActiveSunday = !weekDays[i].isActiveSunday"
-            >
-              {{ weekDays[i].isActiveSunday ? "Fatto!" : "Da fare" }}
-            </button>
-          </td>
-          <td>
-            <button
-              class="btn btn-lg"
-              type="button"
-              v-on:click="deleteRow(i)"
-              aria-label="Delete"
-              title="Delete"
-              style="background-color: transparent;"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M16 1.75V3h5.25a.75.75 0 010 1.5H2.75a.75.75 
-                        0 010-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75zm-6.5 0a.25.25 0 01.25-.25h4.5a.25.25 
-                        0 01.25.25V3h-5V1.75z"
-                ></path>
-                <path
-                  d="M4.997 6.178a.75.75 0 10-1.493.144L4.916 20.92a1.75 1.75 0 001.742 1.58h10.684a1.75 1.75 0 
-                        001.742-1.581l1.413-14.597a.75.75 0 00-1.494-.144l-1.412 14.596a.25.25 0 01-.249.226H6.658a.25.25 0 01-.249-.226L4.997 6.178z"
-                ></path>
-                <path
-                  d="M9.206 7.501a.75.75 0 01.793.705l.5 8.5A.75.75 0 119 16.794l-.5-8.5a.75.75 
-                        0 01.705-.793zm6.293.793A.75.75 0 1014 8.206l-.5 8.5a.75.75 0 001.498.088l.5-8.5z"
-                ></path>
-              </svg>
-            </button>
-            <button
-              class="btn btn-lg"
-              type="button"
-              aria-label="Update"
-              title="Update"
-              style="background-color: transparent;"
-              v-on:click="updateChart(i); countActive(i)"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-arrow-clockwise"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
-                />
-                <path
-                  d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"
-                />
-              </svg>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
-    <div class="opzioni">
-      <button type="button" class="btn btn-lg" @click="addNewRow">
-        <i class="fas fa-plus-circle" style="color: #ffa700"></i>
-        Inserisci una nuova riga
-      </button>
-    </div>
-    <br />
-    <div id="chart">
-      <p style="text-align:center">Queste sono le statistiche delle tue abitudini:</p>
-      <br />
-      <pie-chart :data="datachartByStatus" :donut="true"></pie-chart><br />
-    </div>
-  </div>
-</div>
 </template>
 
 <script>
@@ -434,10 +227,10 @@ export default {
   },
 
   methods: {
-     isMobile() {
+    isMobile() {
       return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
         navigator.userAgent
-      )
+      );
     },
     addNewRow() {
       this.weekDays.push({
@@ -535,12 +328,8 @@ export default {
 </script>
 
 <style>
-.d-flex{
-    display: flex;
-    flex-direction: column;
-}
 .container {
-  width: calc(100%-30%)
+  width:100%
 }
 
 .opzioni button {
@@ -550,6 +339,17 @@ export default {
 
 .green {
   background-color: green;
+}
+
+@media screen and (max-width: 630px) { 
+  .d-flex {
+    flex-direction: column;
+  }
+  .container {
+    width:calc(100% -30%);
+    background-color: white;
+  }
+
 }
 
 
--- src/components/todolist/todo.vue
+++ src/components/todolist/todo.vue
@@ -1,5 +1,5 @@
 <template>
-<div v-if="isMobile()" class= "container">
+<div :class="[isMobile() ? 'container' : '']">
   <div class="m-4" id="todolist">
     <div class="app-title">Cose da fare<span style= "font-size: 35px"> ✏️ </span></div>
     <div>
@@ -220,230 +220,7 @@
   </div>
   <br />
 </div>
-<div v-else> 
-  <div class="m-4" id="todolist">
-    <div class="app-title">Cose da fare<span style= "font-size: 35px"> ✏️ </span></div>
-    <div>
-      <figure style=""><img src="../../assets/prova4.jpg" /></figure>
-    </div>
-    <div class= "descr-todo">
-    Usa questa pagina per appuntarti le cose che non vuoi dimenticarti.
-    <br />
-    Scrivi ciò che vuoi fare oggi e clicca <em>Enter</em> per aggiungere un
-    task<br />
-    alla tua lista di cose da fare.<br />
-    Se vuoi modificare o eliminare un task, basta che clicchi sulle icone a
-    fianco!
-    </div>
-    <br /><br />
-    <div class="empty-state">
-      <h2 class="empty-state__title">Inizia a scrivere la tua todo list!</h2>
-      <p class="empty-state__description">
-        Quali sono i tuoi obiettivi per oggi?
-      </p>
-    </div>
-    <br />
-    <!--Form per l'inserimento degli elementi nella lista-->
-    <!--Controlli sugli elementi della lista-->
-    <div v-if="!isEditing">
-      <div id="form">
-        <input
-          type="text"
-          aria-label="Enter a new todo item"
-          placeholder="Ad esempio: Vai a fare la spesa"
-          v-model="todo"
-          v-on:keyup.enter="storeToDo(todo)"
-        />
-      </div>
-    </div>
-    <div v-else>
-      <div id="form">
-        <input type="text" aria-label="Enter a new todo item" v-model="todo" />
-      </div>
-      <input type="submit" value="Salva" @click="updateToDo()" />
-    </div>
-    <br />
-
-    <h5>Oggi ho deciso di:</h5>
-    <div id="show">
-      <div v-if="todos.length > 0">
-        <div class="form-check form-check-inline">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio1"
-            value="option1"
-            v-on:click="
-              (showUndone = true), (showDone = false), (showAll = false)
-            "
-          />
-          Mostra solo le cose non fatte
-        </div>
-        <div class="form-check form-check-inline">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio2"
-            value="option2"
-            v-on:click="
-              (showDone = true), (showUndone = false), (showAll = false)
-            "
-          />
-          Mostra solo le cose fatte
-        </div>
-        <div class="form-check form-check-inline">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio3"
-            value="option3"
-            v-on:click="
-              (showAll = true), (showDone = false), (showUndone = false)
-            "
-          />
-          Mostra tutto
-        </div>
-      </div>
-      <div v-else>
-        <div class="form-check form-check-inline">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio1"
-            value="option1"
-            disabled
-          />Mostra solo le cose non fatte
-        </div>
-        <div class="form-check form-check-inline">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio2"
-            value="option2"
-            disabled
-          />Mostra solo le cose fatte
-        </div>
-        <div class="form-check form-check-inline">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio3"
-            value="option3"
-            disabled
-          />Mostra tutto
-        </div>
-      </div>
-    </div>
-    <ul>
-      <li v-for="(todo, i) in todosByStatus" :key="i">
-        <input
-          type="checkbox"
-          :id="todo.label"
-          v-on:change="markAsDoneOrUndone(todo.id, todo.done)"
-          v-bind:checked="todo.done"
-        />
-        <label class="label-agree-term" :for="todo.label"
-          ><span></span>{{ todo.label }}
-        </label>
-
-        <div class="actions">
-          <button
-            class="btn btn-lg"
-            type="button"
-            v-on:click="editToDo(i, todo)"
-            style="background-color: transparent;"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              class="bi bi-pencil-square"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 
-                    0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 
-                    .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 
-                    1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 
-                    1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-              />
-            </svg>
-          </button>
-
-          <button
-            class="btn btn-lg"
-            type="button"
-            v-on:click="deleteToDo(todo)"
-            aria-label="Delete"
-            title="Delete"
-            style="background-color: transparent;"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              class="bi bi-trash"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-              />
-            </svg>
-          </button>
-        </div>
-      </li>
-    </ul>
-    <div
-      v-if="
-        todos.filter(todo => {
-          return todo.done === false;
-        }).length == 0
-      "
-    >
-      <p>Non ti rimangono cose da fare oggi, bene! ✔️</p>
-    </div>
-    <div
-      v-else-if="
-        todos.filter(todo => {
-          return todo.done === false;
-        }).length == 1
-      "
-    >
-      <p>Ti rimane una cosa da fare oggi 💪</p>
-    </div>
-    <div v-else>
-      <p>
-        Ti rimangono
-        {{
-          todos.filter(todo => {
-            return todo.done === false;
-          }).length
-        }}
-        cose da fare oggi 💪
-      </p>
-    </div>
-  </div>
-  <br />
-</div>
-
-
-  <!-- Material switch -->
+<!-- Material switch -->
 </template>
 
 <script>
@@ -659,6 +436,9 @@ export default {
 </script>
 
 <style>
+.container {
+  width:100%
+}
 .app-title {
   font-weight: bold;
   font-size: 40px;
@@ -722,6 +502,17 @@ h5 {
   figure {
     width:25%;
   }
+}
+
+@media screen and (max-width: 630px) { 
+  .d-flex {
+    flex-direction: column;
+  }
+  .container {
+    width:calc(100% -30%);
+    background-color: white;
+  }
+
 }
 
 </style>
